b3b0b669cf1fd36782b57540aa64513fec7d60ff
blake <99916948+nilsdeb@users.noreply.github.com>
2023-10-26 10:32:30 +0200

    Update test3.py

--- test/test3.py
+++ test/test3.py
@@ -1,38 +1,11 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+nombreligne = 20
 
-# Liste des coordonnées x, y et z des points
-x_coords = [1, 2, 3, 4, 5]  # Exemple des coordonnées x
-y_coords = [2, 3, 4, 5, 6]  # Exemple des coordonnées y
-z_coords = [3, 4, 5, 6, 7]  # Exemple des coordonnées z
+print('ax=0,ay=0,az=1,gx=0,gy=0,gz=0,lat=45,lon=45')
+print('ax=20,ay=0,az=1,gx=0,gy=0,gz=0,lat=45,lon=45')
+for i in range(nombreligne):
+    print('ax=0,ay=5,az=1,gx=0,gy=0,gz=0,lat=56,lon=46')
 
-# Liste des composantes x, y et z des vecteurs de vitesse
-u_velocities = [0.5, 1, -0.5, 1.5, -1]  # Exemple des composantes x des vecteurs de vitesse
-v_velocities = [1, -0.5, 1.5, -1, 0.5]  # Exemple des composantes y des vecteurs de vitesse
-w_velocities = [-1, 0.5, -1.5, 1, -0.5]  # Exemple des composantes z des vecteurs de vitesse
+#for i in range(4*nombreligne):
+    #print('ax=0,ay=1,az=1,gx=0,gy=0,gz=0,lat=56,lon=46')
 
-# Calcul des vitesses absolues
-velocities = np.sqrt(np.array(u_velocities)**2 + np.array(v_velocities)**2 + np.array(w_velocities)**2)
 
-# Indice du point avec la plus grande vitesse
-max_velocity_idx = np.argmax(velocities)
-
-# Création de la figure
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Tracé des points
-ax.scatter(x_coords, y_coords, z_coords, c='k', marker='o')  # Points en noir
-
-# Tracé du point avec la plus grande vitesse en rouge
-ax.scatter(x_coords[max_velocity_idx], y_coords[max_velocity_idx], z_coords[max_velocity_idx], c='r', marker='o')
-
-# Tracé des vecteurs de vitesse
-ax.quiver(x_coords, y_coords, z_coords, u_velocities, v_velocities, w_velocities)
-
-# Suppression du cadre et des axes
-ax.axis('off')
-
-# Affichage du graphe
-plt.show()
