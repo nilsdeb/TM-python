ea7f2ed5168d8a73426a149e1b0f583743f8a432
blake <99916948+nilsdeb@users.noreply.github.com>
2023-10-30 10:45:08 +0100

    Update test3.py

--- test/test3.py
+++ test/test3.py
@@ -1,9 +1,11 @@
+import math as m
+
 nombreligne = 20
 
 print('ax=0,ay=0,az=1,gx=0,gy=0,gz=0,lat=45,lon=45')
 print('ax=20,ay=0,az=1,gx=0,gy=0,gz=0,lat=45,lon=45')
 for i in range(nombreligne):
-    print('ax=0,ay=5,az=1,gx=0,gy=0,gz=0,lat=56,lon=46')
+    print('ax=0,ay=5,az=1,gx=0,gy=22.5,gz=22.5,lat=56,lon=46')
 
 #for i in range(4*nombreligne):
     #print('ax=0,ay=1,az=1,gx=0,gy=0,gz=0,lat=56,lon=46')
